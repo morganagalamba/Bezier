76ea393d6ff78e08d764aa3ced733c7a7103157f
misabelf <mifs@cin.ufpe.br>
2022-11-07 16:25:14 -0300

    inserção de eventos de mouse e teclado

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
 // PONTO E CURVA 
-
 class Ponto {
  constructor (x, y){
    this.x = x;
    this.y = y;
  }
 }
-
 function interpol (p1, p2, t){
  if(t>=0 && t<=1){
    p = new Ponto;
@@ -16,7 +14,6 @@ function interpol (p1, p2, t){
  }else return -1;
    
  }
-
 function deCasteljau(pontos, t) {
   if (pontos.length == 2) {
   return interpol(pontos[0], pontos[1], t)
@@ -31,29 +28,47 @@ function deCasteljau(pontos, t) {
 }
 
 // ELEMENTOS - html
+var canvas = document.getElementById("canvas");
+var canvas2d = canvas.getContext("2d");
+//toggle
+var togglePontos = document.getElementById("togglePontos");
+var togglePoligonais = document.getElementById("togglePoligonais");
+var toggleCurvas = document.getElementById("toggleCurvas");
+//botoes
+var adcCurva = document.getElementById("adicionaCurva");
+var rmvCurva = document.getElementById("removeCurva");
+var adcPonto = document.getElementById("adicionaPonto");
+//var rmvPonto = document.getElementById("removePonto");
+var curvaAnterior = document.getElementById("anterior");
+var curvaPosterior = document.getElementById("posterior");
+var pontoAnterior = document.getElementById("pontoAnterior");
+var pontoPosterior = document.getElementById("pontoPosterior");
+var avaliacoes = document.getElementById("avaliacoesCurva");
+var mensagem = document.getElementById("mensagem");
+
 
 // VARIAVEIS E CONSTANTES
-const RAIO_PONTO = 6;
+const RAIO_PONTO = 30;
 var curvas = [];
 curvas.push([]);
 var curvaSelecionada = -1;
 var pontoSelecionado = [];
 pontoSelecionado.push(0);
-var numAvaliacoes = 100;
+var numAvaliacoes = 200;
 // do canvas
 var clique = false;
 var pontosAparentes = true;
 var poligonaisAparentes = true;
 var curvasAparentes = true;
+var estadoCanvas = 0;
+
 
 // FUNÇOES DE DESENHO
-
 function desenharPonto(p1) {
   canvas2d.beginPath();
   canvas2d.arc(p1.x, p1.y, RAIO_PONTO, 0, 2 * Math.PI);
-   canvas2d.stroke();
+  canvas2d.stroke();
 }
-
 function desenharReta(p1, p2){
   canvas2d.beginPath();
   canvas2d.lineTo(p1.x, p1.y);
@@ -63,12 +78,10 @@ function desenharReta(p1, p2){
 }
 function desenharPoligonos(pontos){
   let i = 0;
-  for(i; i < pontos.lenght -1; i++){
+  for(i; i < pontos.lenght - 1; i++){
     desenharReta(pontos[i], pontos[i+1]);
   }
 }
-
-
 function desenharCurva(curva) {
   if(curva.lenght > 2){
     var bezier = [];
@@ -77,13 +90,12 @@ function desenharCurva(curva) {
     for(i; i<= numAvaliacoes; i++){
       bezier.push(deCasteljau(curva, i / numAvaliacoes));
     }
-    bezier.push(curva[curva.lenght -1]);
+    bezier.push(curva[curva.lenght - 2]);
     desenharPoligonos(bezier);
   }
 }
-
-function redesenharCurva() {
- // canvas2d.clearRect(0, 0, width, height); aqui precisa da var canvas do html
+function redesenhar() {
+  canvas2d.clearRect(0, 0, width, height);
   if(pontosAparentes){
     let i = 0;
     for(i; i < curvas.length; i++){
@@ -94,7 +106,7 @@ function redesenharCurva() {
         } else {
           canvas2d.strokeStyle = "green";
         }
-        desenharPonto(curvar[i][j]);
+        desenharPonto(curvas[i][j]);
       }
       
     }
@@ -121,6 +133,103 @@ function redesenharCurva() {
       desenharCurva(curvas[i]);
     }
   }
-
-  
   }
+
+//CANVAS EVENTS
+canvas.addEventListener("mousemove", function(event){
+  if(clique){
+    var p1 = new Ponto(event.offsetX, event.offsetY);
+    if(estadoCanvas == 1){
+      curvas[curvaSelecionada].splice(curvas[curvaSelecionada].lenght -1, 1, p1);
+    }else if(estadoCanvas == 2) {
+      curvas[curvaSelecionada].splice(pontoSelecionado[curvaSelecionada], 1, p1);
+    }
+    redesenhar();
+  }
+});
+canvas.addEventListener("mousedown", function(event){
+  clique = true;
+  var p1 = new Ponto(event.offsetX, event.offsetY);
+  if(estadoCanvas == 1){
+    curvas[curvaSelecionada].push(p1);
+  }else if (estadoCanvas == 2) {
+    curvas[curvaSelecionada].splice(pontoSelecionado[curvaSelecionada], 1, p1);
+  }
+  redesenhar();
+});
+canvas.addEventListener("mouseup", function(event){
+  clique = false;
+  redesenhar();
+});
+
+//FUNCIOALIDADE BOTOES
+//curva
+adcCurva.addEventListener("click", function(event){
+  if((curvaSelecionada == -1 || curvas[curvaSelecionada].lenght>1)){
+    estadoCanvas = 1;
+    var novaCurva= [];
+    curvas.push(novaCurva);
+    pontoSelecionado.push(0);
+    curvaSelecionada++;
+    mensagem.innerText = "clique na tela para adicionar pontos de controle"
+  }else{
+    mensagem.innerText = "não é possivel adicionar uma nova curva antes de finalizar a atual"
+  }
+});
+rmvCurva.addEventListener("click", function(event){
+  if(curvas.length > 0) {
+    curvas.splice(curvaSelecionada, 1);
+    pontoSelecionado.splice(curvaSelecionada, 1);
+    if(curvaSelecionada > 0){
+      curvaSelecionada--;
+    };
+  }
+  redesenhar();
+});
+curvaAnterior.addEventListener("click", function(event){
+  if(curvaSelecionada > 0){
+    curvaSelecionada--;
+  }
+  redesenhar();
+});
+curvaPosterior.addEventListener("click", function(event){
+  if(curvaSelecionada < curvas.length - 2){
+    curvaSelecionada++;
+  }
+  redesenhar();
+});
+//pontos
+adcPonto.addEventListener("click", function(event){
+  estadoCanvas = 1;
+  mensagem.innerText = "clique na tela para adicionar pontos de controle";
+});
+pontoAnterior.addEventListener("click", function(event){
+  if(pontoSelecionado[curvaSelecionada] > 0){
+    pontoSelecionado[curvaSelecionada]--;
+    redraw();
+  }else redraw();
+});
+pontoPosterior.addEventListener("click", function(event){
+  if(pontoSelecionado[curvaSelecionada] < pontoSelecionado[curvaSelecionada].lenght - 1){
+    pontoSelecionado[curvaSelecionada]++;
+    redraw();
+  }else redraw();
+});
+//toggles
+togglePontos.addEventListener("click", function(event){
+  pontosAparentes = !pontosAparentes;
+  redesenhar();
+});
+togglePoligonais.addEventListener("click", function(event){
+  poligonaisAparentes = !poligonaisAparentes;
+  redesenhar();
+});
+toggleCurvas.addEventListener("click", function(event){
+  curvasAparentes = !curvasAparentes;
+  redesenhar();
+});
+avaliacoes.addEventListener("keyup", function(event){
+  var entrada = event.target.value;
+  numAvaliacoes = parseInt(entrada);
+  redesenhar();
+})
